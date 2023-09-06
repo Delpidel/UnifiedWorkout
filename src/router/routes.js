f8dfdb749b8bb78044440dba77e0b7ca79d34330
Helena Delpizzo <hdelpizzo@gmail.com>
2023-09-05 21:35:41 -0300

    feat: importação de rotas em routes.js

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,12 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-import Login from '../views/Login/Login.vue'
-import SignUp from '..views/SignUp/SignUp.vue'
-import Dashboard from '../views/Dashboard/Dashboard.vue'
-import ManageExercises from '../views/ManageExercises/Exercises.vue'
-import ManageStudents from '../views/ManageStudents/Students.vue'
-import NewStudent from '../views/NewStudent/NewStudent.vue'
-import NewWorkout from '../views/NewWorkout/NewWorkout.vue'
+import Login from '../views/login/Login.vue'
+import SignUp from '..views/cadastro/SignUp.vue'
+import Dashboard from '../views/dashboard/Dashboard.vue'
+import ManageExercises from '../views/gerenciamento-exercicios/Exercises.vue'
+import ManageStudents from '../views/gerenciamento-alunos/Students.vue'
+import NewStudent from '../views/novo-aluno/NewStudent.vue'
+import NewWorkout from '../views/cadastro-exercicios/NewWorkout.vue'
+import WorkoutView from '../views/visualizar-exercicios/WorkoutView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -27,7 +28,29 @@ const router = createRouter({
       component: Dashboard
     },
     {
-      path: '/'
+      path: '/gerenciamento-exercicios',
+      name: 'Exercises',
+      component: ManageExercises
+    },
+    {
+      path: '../gerenciamento-alunos',
+      name: 'Students',
+      component: ManageStudents
+    },
+    {
+      path: '../novo-aluno',
+      name: 'NewStudent',
+      component: NewStudent
+    },
+    {
+      path: '../cadastro-exercicios',
+      name: 'NewWorkout',
+      component: NewWorkout
+    },
+    {
+      path: '../visualizar-exercicios',
+      name: 'WorkoutView',
+      component: WorkoutView
     }
   ]
 })
